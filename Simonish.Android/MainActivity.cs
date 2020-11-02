ce02a4fa61599413974a28261c4054c5fcfa7617
Rhys Jones <rhys@rmjcs.com>
2020-11-02 15:12:51 +0000

    Set the MainActivity [LaunchMode](https://developer.android.com/guide/topics/manifest/activity-element#lmode) to SingleTask. Android will now only create one instance of the activity.  This further helps issue #004.

--- Simonish.Android/MainActivity.cs
+++ Simonish.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using Xamarin.Essentials;
 
 namespace Rmjcs.Simonish.Droid
 {
-    [Activity(Label = "Simonish", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "Simonish", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTask, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         private App _app;
